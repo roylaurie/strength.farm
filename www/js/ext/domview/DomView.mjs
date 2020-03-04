36b7e4c8c62ddabf7a0e39594104e1dfafaad908
roylaurie <roy.laurie@gmail.com>
2020-03-04 14:59:49 -0800

    domview refactor

--- www/js/ext/domview/DomView.mjs
+++ www/js/ext/domview/DomView.mjs
@@ -1,5 +1,7 @@
 'use strict';
 
+import DomVariablePointer from "www/js/ext/domview/DomVariablePointer.mjs";
+
 /**
  * Maintains an offscreen copy of the DOM for a specific template instance.
  * Binds values.
@@ -15,22 +17,20 @@ export default class DomView {
         this._varPointers = {};
         this._built = false;
 
-        // build template pointers for the offscreen copy of the dom
-        let pointers = template.getTemplatePointers();
-        this._dom.querySelectorAll('[data-template]').forEach((node) => {
-            let name = node.getAttribute('data-template');
-            if (typeof pointers[name] === 'undefined') {
-                throw new Error('Unable to find pointer for template ' + name);
-            }
+        this._compile();
+    }
 
-            let templatePointer = pointers[name];
-            this._templatePointers[name] = new DomViewPointer(node, templatePointer.getTemplate());
+    _compile() {
+        // build template pointers for the offscreen copy of the dom
+        this._dom.querySelectorAll('[data-template]').forEach((node) => {
+            let templateName = node.getAttribute('data-template');
+            this._templatePointers[templateName] = new DomViewTemplatePointer(node, this);
         });
 
         // build variable pointers for the dom copy
         this._dom.querySelectorAll('[data-tmpl-var]').forEach((node) => {
             let varName = node.getAttribute('data-tmpl-var');
-            this._varPointers[varName] = new DomVariablePointer(node, varName);
+            this._varPointers[varName] = new DomVariablePointer(node, this, varName);
         });
     };
 
@@ -68,21 +68,7 @@ export default class DomView {
             varPointer.bindValue(value);
         }
 
-        return this; //todo
-
-        // fill any attributes specified
-        if (typeof values['attr'] !== 'undefined') {
-            for (let name in values['attr']) {
-                node.setAttribute(varName, values['attr'][varName]);
-            }
-        }
-
-        // fill innerText or innerHTML if specified
-        if (typeof values['innerText'] !== 'undefined') {
-            node.innerText = values['innerText'];
-        } else if (typeof values['innerHTML'] !== 'undefined') {
-            node.innerHTML = values['innerHTML'];
-        }
+        return this;
     };
 
     insertView(viewName) {
@@ -98,11 +84,26 @@ export default class DomView {
         return template;
     };
 
-    build() {
+    display() {
         const stripAttributes = ['data-tmpl-var'];
 
-        if (this._built) {
-            throw new Error('View already built.');
+        // fill variable values
+        for (let varName in this._varPointers) {
+            let varPointer = this._varPointers[varName];
+
+            // fill any attributes specified
+            if (typeof values['attr'] !== 'undefined') {
+                for (let name in values['attr']) {
+                    node.setAttribute(varName, values['attr'][varName]);
+                }
+            }
+
+            // fill innerText or innerHTML if specified
+            if (typeof values['innerText'] !== 'undefined') {
+                node.innerText = values['innerText'];
+            } else if (typeof values['innerHTML'] !== 'undefined') {
+                node.innerHTML = values['innerHTML'];
+            }
         }
 
         // remove all DomView attributes from offscreen dom copy
